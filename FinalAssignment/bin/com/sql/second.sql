d56381f42abda3f3cdd398ba5eca0c9b9ce4ddd7
Diwash Mainali <diwashmainali900@gmail.com>
2023-01-23 15:35:34 +0545

    Normalized sql query written

---
+++ FinalAssignment/bin/com/sql/second.sql
@@ -0,0 +1,121 @@
+CREATE TABLE Instructor (
+  Instructor_ID varchar(255) PRIMARY KEY,
+  Name varchar(255),
+  Address varchar(255),
+  Year_of_experience varchar(255),
+  Citizenship_Number varchar(255),
+  Pan_number varchar(255),
+  DOB varchar(255),
+  Email_address varchar(255),
+  Password varchar(255)
+);
+
+CREATE TABLE Courses (
+  Course_ID varchar(255) PRIMARY KEY,
+  Course_name varchar(255)
+);
+
+CREATE TABLE Module (
+  Course_ID varchar(255),
+  Module_name varchar(255),
+  Module_code varchar(255),
+  PRIMARY KEY (Course_ID, Module_code),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Student (
+  Student_ID varchar(255) PRIMARY KEY,
+  Name varchar(255),
+  Address varchar(255),
+  DOB varchar(255),
+  Citizenship_Number varchar(255),
+  Email_address varchar(255),
+  College_email_address varchar(255),
+  College_email_address_password varchar(255),
+  Course_ID varchar(255),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Assignment (
+  Course_ID varchar(255),
+  Module_code varchar(255),
+  Assignment_Number varchar(255),
+  Student_ID varchar(255),
+  Instructor_ID varchar(255),
+  Submitted varchar(255),
+  PRIMARY KEY (Course_ID, Module_code, Assignment_Number),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+  FOREIGN KEY (Instructor_ID) REFERENCES Instructor(Instructor_ID)
+);
+
+CREATE TABLE Course_Administrator (
+  login_email varchar(255) PRIMARY KEY,
+  login_psw varchar(255)
+);
+
+CREATE TABLE Instructor_Module (
+  Instructor_ID varchar(255),
+  Module_code varchar(255),
+  PRIMARY KEY (Instructor_ID, Module_code),
+  FOREIGN KEY (Instructor_ID) REFERENCES Instructor(Instructor_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code)
+);
+
+CREATE TABLE Student_Module (
+  Student_ID varchar(255),
+  Module_code varchar(255),
+  PRIMARY KEY (Student_ID, Module_code),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code)
+);
+
+CREATE TABLE Student_Course (
+  Student_ID varchar(255),
+  Course_ID varchar(255),
+  PRIMARY KEY (Student_ID, Course_ID),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Instructor_Course (
+  Instructor_ID varchar(255),
+  Course_ID varchar(255),
+  PRIMARY KEY (Instructor_ID, Course_ID),
+  FOREIGN KEY (Instructor_ID) REFERENCES Instructor(Instructor_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Assignment_Module (
+  Course_ID varchar(255),
+  Module_code varchar(255),
+  Assignment_Number varchar(255),
+  PRIMARY KEY (Course_ID, Module_code, Assignment_Number),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code)
+);
+
+CREATE TABLE Assignment_Student (
+  Course_ID varchar(255),
+  Module_code varchar(255),
+  Assignment_Number varchar(255),
+  Student_ID varchar(255),
+  PRIMARY KEY (Course_ID, Module_code, Assignment_Number, Student_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code),
+  FOREIGN KEY (Assignment_Number) REFERENCES Assignment(Assignment_Number),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
+);
+
+CREATE TABLE Assignment_Instructor (
+  Course_ID varchar(255),
+  Module_code varchar(255),
+  Assignment_Number varchar(255),
+  Instructor_ID varchar(255),
+  PRIMARY KEY (Course_ID, Module_code, Assignment_Number, Instructor_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
+  FOREIGN KEY (Module_code) REFERENCES Module(Module_code),
+  FOREIGN KEY (Assignment_Number) REFERENCES Assignment(Assignment_Number),
+  FOREIGN KEY (Instructor_ID) REFERENCES Instructor(Instructor_ID)
+);
