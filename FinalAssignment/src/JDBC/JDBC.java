7e9dd7370b332e88cd1878c348a5ae4f44c0e93f
Diwash Mainali <diwashmainali900@gmail.com>
2023-01-23 10:14:31 +0545

    initial commit

--- FinalAssignment/src/JDBC/JDBC.java
+++ FinalAssignment/src/JDBC/JDBC.java
@@ -1,14 +1,14 @@
-package JDBC;
+// package JDBC;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
 
-public class JDBC {
-	public static void main(String[] args) {
-		try {
-			DriverManager.getConnection("jdbc:mysql://localhost:3306/ ","root","");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-}
+// public class JDBC {
+// 	public static void main(String[] args) {
+// 		try {
+// 			DriverManager.getConnection("jdbc:mysql://localhost:3306/ ","root","");
+// 		} catch (SQLException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// }
---
+++ FinalAssignment/src/com/database/JDBC.java
@@ -0,0 +1,14 @@
+package com.database;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+	public static void main(String[] args) {
+		try {
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/ ","root","");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
