6544d3d0deb513537b78a7b2571bfbc31fe7bc3f
Diwash Mainali <diwashmainali900@gmail.com>
2023-01-24 00:18:47 +0545

    Database connected successfully

--- FinalAssignment/src/com/database/JDBC.java
+++ FinalAssignment/src/com/database/JDBC.java
@@ -1,37 +1,14 @@
 package com.database;
 
-// import java.sql.DriverManager;
-// import java.sql.SQLException;
-
-// public class JDBC {
-// 	public static void main(String[] args) {
-// 		try {
-// 			DriverManager.getConnection("jdbc:mysql://localhost:3306/course_manaagement_system ","root","");
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
-// }
-
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JDBC {
-    private Connection connection;
-
-    public JDBC() {
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://hostname:port/dbname", "username", "password");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public ResultSet executeQuery(String query) throws SQLException {
-        Statement stmt = connection.createStatement();
-        return stmt.executeQuery(query);
-    }
-
-    public void close() throws SQLException {
-        connection.close();
-    }
+	public static void main(String[] args) {
+		try {
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/course_manaagement_system ","root","");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
