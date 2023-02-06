c7b1e63b6890892316aa8673a4f03bcea96f3ccc
Diwash Mainali <diwashmainali900@gmail.com>
2023-02-06 09:48:10 +0545

    Student Code commented

--- FinalAssignment/src/com/frontend/Student.java
+++ FinalAssignment/src/com/frontend/Student.java
@@ -1239,9 +1239,7 @@ public class Student extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Student.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Student().setVisible(true);
